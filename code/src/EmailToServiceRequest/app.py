d28edeb760a4cafcd126ca9567aaee74674e3d40
Swagatika Samantaray <swagatika.samantaray3737@gmail.com>
2025-03-26 10:51:54 +0530

    Model change

--- code/src/EmailToServiceRequest/app.py
+++ code/src/EmailToServiceRequest/app.py
@@ -12,6 +12,10 @@ import io
 from email import message_from_file
 from email.policy import default
 from email.utils import parseaddr
+from llama_cpp import Llama
+from gpt4all import GPT4All 
+from collections import Counter
+import json
 
 app = Flask(__name__)
 
@@ -194,6 +198,12 @@ def parse_eml_and_attachments(file_path):
             os.remove(attach_file_path)
 
     return email_info
+def read_docx_no_paragraphs(file_path):
+    doc=Document(file_path)
+    return " ".join([para.text.strip() for para in doc.paragraphs if para.text.strip()])
+
+def split_text(text,max_length=400):
+    return [text[i:i+max_length]for i in range(0,len(text),max_length)]
 
 @app.route('/process/file', methods=['POST'])
 def upload_file():
@@ -215,6 +225,38 @@ def upload_file():
 
             if os.path.exists(file_path):
                 os.remove(file_path)
+            model_file="Meta-Llama-3-8B-Instruct-Q4_0.gguf"
+            model_folder = r"C:\Users\padhideb"
+            model_path = os.path.join(model_folder,model_file)
+            # file_name="Invoice.docx"
+            # file_path = os.path.join(model_folder,file_name)
+            llm=Llama(model_path)
+            document_text = read_docx_no_paragraphs(email_info.body)
+            chunks = split_text(document_text,max_length=400)
+
+            few_shot_prompt = (
+                "You are an expert at classifying documents by their request type."
+                "Based on the content, assign one of the following labels:Finacial, Techincal, or Legal.\n\n"
+                "Return only Answer with no explanation.\n\n"
+                "Example 1:\n"
+                "Document:Reason for transferring funds\n"
+                "Request Type:Financial\n\n"
+                "Example 2:\n"
+                "Document:API integration guide explaining authentication procedures.\n"
+                "Request Type:Technical\n\n"
+                "Now classify the following document.\n"
+                "Document:{document_chunk}\n"
+                "Request Type:"
+            )
+            results = []
+            for chunk in chunks:
+                prompt = few_shot_prompt.format(document_chunk=chunk)
+                response=llm(prompt)
+                answer = response["choices"][0]["text"].strip()
+                results.append(answer)
+
+            final_classification = Counter(results).most_common(1)[0][0]
+            output = {"request_type":final_classification}
 
             return jsonify({"email_info": email_info}), 200
         except Exception as e:
@@ -248,4 +290,4 @@ def upload_eml_directory():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000,debug=True)
